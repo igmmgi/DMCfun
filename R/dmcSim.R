1f7d049511727a01f5ff13d44170e010d976eb06
ian <ian.mackenzie@uni-tuebingen.de>
2020-02-20 11:59:37 +0100

    remove leftover debug code

--- R/dmcSim.R
+++ R/dmcSim.R
@@ -91,25 +91,18 @@ dmcSim <- function(amp = 20, tau = 30, mu = 0.5, bnds = 75, resMean = 300, resSD
   # summary
   summary <- dmc$summary 
   
+  
   dmc$means <- tibble::as_tibble(rbind(summary$resSum_comp, summary$resSum_incomp), .names_repair = "minimal")
   colnames(dmc$means) <- c("rtCor", "sdRtCor", "perErr", "rtErr", "sdRtErr")
   dmc$means <- tibble::add_column(Comp = c("comp", "incomp"), dmc$means, .before = TRUE)
 
   # caf
-  if(length(summary$caf_comp) != 5) {
-    save(dmc, file = "xy.Rdata")
-    stop()
-  }
   nCAF    <- length(summary$caf_comp)
   dmc$caf <- tibble::tibble(accPer = c(summary$caf_comp, summary$caf_incomp))
   dmc$caf <- tibble::add_column(bin = rep(1:nCAF, each = 1, times = 2), dmc$caf, .before = TRUE)
   dmc$caf <- tibble::add_column(Comp = rep(c("comp", "incomp"), each = nCAF), dmc$caf, .before = TRUE)
 
   # delta
-  if(length(summary$delta_pct_comp) != 19) {
-    save(dmc, file = "xy.Rdata")
-    stop()
-  }
   nDelta    <- length(summary$delta_pct_comp)
   dmc$delta <- tibble::tibble("meanComp" = summary$delta_pct_comp,
                               "meanIncomp" = summary$delta_pct_incomp,
