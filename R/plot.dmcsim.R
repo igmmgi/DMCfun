4d82caa172f8dc4893bf9659c18f13f40abfae0c
ian <ian.mackenzie@uni-tuebingen.de>
2020-03-13 13:26:26 +0100

    remove debug message

--- R/plot.dmcsim.R
+++ R/plot.dmcsim.R
@@ -70,7 +70,7 @@ plot.dmcsim <- function(x,
   if (figType %in% c("trials", "activation") & !("trials" %in% names(x))) {
     stop("plotting trials/activation data requires dmcSim with fullData = TRUE")
   }
-  if (!(is.function(legend)) && !(legend %in% c(TRUE, FALSE))) {
+  if (!is.function(legend) && !legend %in% c(TRUE, FALSE)) {
     stop("legend must be TRUE/FALSE or a function")
   } 
    
@@ -131,7 +131,6 @@ plot.dmcsim <- function(x,
     lines(c(0, x$prms$tmax), c( x$prms$bnds,  x$prms$bnds), type = "l", col = "darkgrey", ...)
     lines(c(0, x$prms$tmax), c(-x$prms$bnds, -x$prms$bnds), type = "l", col = "darkgrey", ...)
     
-    message(is.function(legend))
     if (is.function(legend)) {
       legend()
     } else if (!is.list(legend) && legend == TRUE) {
