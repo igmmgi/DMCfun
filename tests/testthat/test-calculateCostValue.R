f95adff1844dd351a8749ec40f2bc80ebb14f6c4
ian <ian.mackenzie@uni-tuebingen.de>
2021-06-04 14:50:41 +0200

    RMSE

--- tests/testthat/test-calculateCostValue.R
+++ tests/testthat/test-calculateCostValue.R
@@ -7,14 +7,14 @@ test_that("calculateCostValue", {
   # test 1:
   resTh <- dmcSim()
   resOb <- dmcSim()
-  cost  <- calculateCostValue(resTh, resOb)
+  cost  <- calculateCostValueRMSE(resTh, resOb)
 
   testthat::expect_lt(cost, 3)
 
   # test 2:
   resTh <- dmcSim()
   resOb <- dmcSim(tau = 150)
-  cost  <- calculateCostValue(resTh, resOb)
+  cost  <- calculateCostValueRMSE(resTh, resOb)
 
   testthat::expect_gt(cost, 20)
 
