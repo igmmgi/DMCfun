0db9208d89a710c7fcd8b857fe920c8a9f43df2b
ian <ian.mackenzie@uni-tuebingen.de>
2021-06-04 14:50:17 +0200

    :::

--- tests/testthat/test-errDist.R
+++ tests/testthat/test-errDist.R
@@ -5,13 +5,13 @@ test_that("errDist", {
   set.seed(1)
   
   # test 1: 10% errors
-  errs <- errDist(10000, 10)
+  errs <- DMCfun:::errDist(10000, 10)
 
   testthat::expect_equal(length(errs), 10000)
   testthat::expect_equal(0.1, round(mean(errs), 1))
 
   # test 2: 20% errors 
-  errs <- errDist(10000, 20)
+  errs <- DMCfun:::errDist(10000, 20)
 
   testthat::expect_equal(length(errs), 10000)
   testthat::expect_equal(0.2, round(mean(errs), 1))
--- tests/testthat/test-rtDist.R
+++ tests/testthat/test-rtDist.R
@@ -5,12 +5,12 @@ test_that("rtDist", {
   set.seed(1)
   
   # test 1
-  x <- DMCfun::rtDist()
+  x <- DMCfun:::rtDist()
   testthat::expect_equal(length(x), 10000)
   testthat::expect_equal(600, round(mean(x)))
 
   # test 2
-  x <- DMCfun::rtDist(n = 100000, gaussMean = 500)
+  x <- DMCfun:::rtDist(n = 100000, gaussMean = 500)
   testthat::expect_equal(length(x), 100000)
   testthat::expect_equal(500, round(mean(x)))
 
