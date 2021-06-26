69699aa27dbf007b0cc9ebd259dac904e86e5114
igm <ian.mackenzie@uni-tuebingen.de>
2021-06-26 12:35:31 +0200

    formatting

--- src/inDMC.cpp
+++ src/inDMC.cpp
@@ -3,11 +3,11 @@
 #include "inDMC.h"
 
 void print_input_args(Prms &p) {
-    Rprintf("\nDMC Parameters:\n");
-    Rprintf("amp: %-8.2f tau: %-8.2f aaShape: %-8.2f drc: %-8.2f bnds: %-8.2f resMean: %-8.2f resSD: %-8.2f sigm: %-8.2f", 
+    p.printResults ? Rprintf("\nDMC Parameters:\n") : Rprintf("\n");
+    Rprintf("amp: %-5.1f tau: %-4.0f aaShape: %-5.2f drc: %-5.2f bnds: %-3.0f resMean: %-4.0f resSD: %-3.0f sigm: %-5.2f", 
             p.amp, p.tau, p.aaShape, p.drc, p.bnds, p.resMean, p.resSD, p.sigm) ;
-    if (p.varSP) Rprintf("spShape: %-8.2f", p.spShape);
-    if (p.varDR) Rprintf("drShape: %-8.2f", p.drShape);
-    Rprintf("\n\n");
+    if (p.varSP) Rprintf("spShape: %-5.2f", p.spShape);
+    if (p.varDR) Rprintf("drShape: %-5.2f", p.drShape);
+    if (p.printResults) Rprintf("\n\n");
 }
 
