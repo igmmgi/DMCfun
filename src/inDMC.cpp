b51fcb1221a120e39c9aa0ed583e06a5cc4a0c00
ian <ian.mackenzie@uni-tuebingen.de>
2021-07-07 15:26:11 +0200

    print order

--- src/inDMC.cpp
+++ src/inDMC.cpp
@@ -4,8 +4,8 @@
 
 void print_input_args(Prms &p) {
     p.printResults ? Rprintf("\nDMC Parameters:\n") : Rprintf("\n");
-    Rprintf("amp: %-5.1f tau: %-4.0f aaShape: %-5.2f drc: %-5.2f bnds: %-3.0f resMean: %-4.0f resSD: %-3.0f sigm: %-5.2f", 
-            p.amp, p.tau, p.aaShape, p.drc, p.bnds, p.resMean, p.resSD, p.sigm) ;
+    Rprintf("amp: %-5.1f tau: %-4.0f drc: %-5.2f bnds: %-3.0f resMean: %-4.0f resSD: %-3.0f aaShape: %-5.2f sigm: %-5.2f", 
+            p.amp, p.tau, p.drc, p.bnds, p.resMean, p.resSD, p.aaShape, p.sigm) ;
     if (p.varSP) Rprintf("spShape: %-5.2f", p.spShape);
     if (p.varDR) Rprintf("drShape: %-5.2f", p.drShape);
     if (p.printResults) Rprintf("\n\n");
