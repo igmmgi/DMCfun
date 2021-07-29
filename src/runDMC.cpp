b6d66d2517a55f40e1624325d8367b144d2ed54c
ian <ian.mackenzie@uni-tuebingen.de>
2021-07-29 13:57:44 +0200

    spBias

--- src/runDMC.cpp
+++ src/runDMC.cpp
@@ -112,10 +112,10 @@ void variable_drift_rate(Prms &p, std::vector<double> &dr, RNG &rng) {
 void variable_starting_point(Prms &p, std::vector<double> &sp, RNG &rng) {
   if (p.spDist == 1) {
     boost::random::beta_distribution<double> bdSP(p.spShape, p.spShape);
-    for (auto &i : sp) i = bdSP(rng) * (p.spLimHigh - p.spLimLow) + p.spLimLow;
+    for (auto &i : sp) i = (bdSP(rng) * (p.spLimHigh - p.spLimLow) + p.spLimLow) + p.spBias;
   } else if (p.spDist == 2) {
     boost::random::uniform_real_distribution<double> unSP(p.spLimLow, p.spLimHigh);
-    for (auto &i : sp) i = unSP(rng);
+    for (auto &i : sp) i = unSP(rng) + p.spBias;
   }
 }
 
