ab87b10ec609a5c0689dc798ef8e3b4ccdf93a2f
ian <ian.mackenzie@uni-tuebingen.de>
2024-01-09 10:17:07 +0100

    CRAN warning fixed

--- src/inDMC.cpp
+++ src/inDMC.cpp
@@ -4,11 +4,10 @@
 void print_input_args(Prms &p) {
   p.printResults ? Rprintf("\nDMC Parameters:\n") : Rprintf("\n");
 
-  Rprintf("amp:%-5.1f tau:%-4.0f drc:%-5.2f bnds:%-3.0f bndsRate:%-0.2f bndsSaturation:%-4.0f resMean:%-4.0f "
+  Rprintf("amp:%-5.1f tau:%-4.0f drc:%-5.2f bnds:%-3.0f bndsRate:%-.2f bndsSaturation:%-4.0f resMean:%-4.0f "
             "resSD:%-3.0f aaShape:%-4.1f spShape:%-4.1f spBias:%-4.1f sigm:%-4.1f",
             p.amp, p.tau, p.drc, p.bnds, p.bndsRate, p.bndsSaturation, p.resMean, p.resSD, p.aaShape, p.spShape,
             p.spBias, p.sigm);
 
-  if (p.printResults)
-    Rprintf("\n\n");
+  if (p.printResults) Rprintf("\n\n");
 }
